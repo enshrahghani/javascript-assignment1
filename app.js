bc31dafa18e68e02feffb7cd4992c775c94243fe
Shahjahan M.Aslam <shahjahanmuhammadaslam@gmail.com>
2022-10-31 12:25:26 +0500

    js assignment 1

---
+++ app.js
@@ -0,0 +1,64 @@
+alert(
+    "hey there"
+)
+
+
+var abc = "ENSHRAH HERE"
+alert(abc)
+
+
+var a = 90
+var b = 10
+var c = a + b
+
+alert(c)
+
+
+var d = "Hello"
+var e = "java"
+var f = d + e
+
+alert(f)
+
+
+// var abc = legal
+// var abc123 = legal
+// var abc_123 = legal
+
+// var var = illegal
+// var @abc = illegal
+// var +abc = illegal
+
+
+alert(10 + 10);
+var popularnumber = 30 - 20;
+alert(popularnumber)
+alert(10 * 10)
+
+
+var w = 10;
+w++;
+alert(w)
+
+var x = 10;
+x--
+alert(x)
+
+var y = 10;
+++y
+console.log(y);
+ 
+var z = 10
+--z
+console.log(z)
+
+
+
+var sum = (10 + 10) -4 *2
+alert(sum)
+
+var deduct = 56 * 89 * 90 -345
+console.log(deduct)
+
+
+
